8a0f831d74c6894883fc6da05bfaeae23b9dadf7
nghuyen dinh quoc uyy <90595569+tonyuyndqOM@users.noreply.github.com>
2024-08-19 21:58:10 +0700

    pages yay

---
+++ pages.js
@@ -0,0 +1,24 @@
+var pages = [
+`Addiction: Social media addiction has become a serious problem among students. Many students spend hours on social media platforms, which affects their academic performance.
+<br><br>Cyberbullying: Social media has made it easier for bullies to harass and intimidate their victims. Cyberbullying can lead to depression, anxiety, and even suicide.
+<br><br>Distraction: Social media can be a major distraction for students. It can affect their concentration and make it difficult for them to focus on their studies.
+<br><br>False Information: Social media is flooded with false information, which can be misleading for students. They may end up believing in false information and make decisions based on it.
+<br><br>Enhances Communication: Social media has made communication easier for students. It has created a platform where students can easily communicate with their peers and teachers, exchange information, and collaborate on projects.
+<br><br>Information Sharing: Social media platforms like Facebook, Twitter, and Instagram have become valuable sources of information. Students can access news and information about events and activities that are happening around the world.
+<br><br>Educational Opportunities: Social media has made it easier for students to access educational resources. Online platforms like YouTube, Coursera, and Khan Academy provide free access to educational content.
+<br><br>Networking Opportunities: Social media has created networking opportunities for students. They can connect with professionals and experts in their fields of study, share their ideas and projects, and get feedback.`,
+`Aalbers et al. (2018) reported that individuals who spent more time passively working with social media suffered from more intense levels of hopelessness, loneliness, depression, and perceived inferiority. For another, Tang et al. (2013) observed that the procedures of sharing information, commenting, showing likes and dislikes, posting messages, and doing other common activities on social media are correlated with higher stress. Similarly, Ley et al. (2014) described that people who spend 2 h, on average, on social media applications will face many tragic news, posts, and stories which can raise the total intensity of their stress. This stress-provoking effect of social media has been also pinpointed by Weng and Menczer (2015), who contended that social media becomes a main source of stress because people often share all kinds of posts, comments, and stories ranging from politics and economics, to personal and social affairs. According to Iwamoto and Chun (2020), anxiety and depression are the negative emotions that an individual may develop when some source of stress is present. In other words, when social media sources become stress-inducing, there are high chances that anxiety and depression also develop.
+Charoensukmongkol (2018) reckoned that the mental health and well-being of the global population can be at a great risk through the uncontrolled massive use of social media. These researchers also showed that social media sources can exert negative affective impacts on teenagers, as they can induce more envy and social comparison. According to Fleck and Johnson-Migalski (2015), though social media, at first, plays the role of a stress-coping strategy, when individuals continue to see stressful conditions (probably experienced and shared by others in media), they begin to develop stress through the passage of time. Chukwuere and Chukwuere (2017) maintained that social media platforms continue to be the major source of changing mood among general populations. For example, someone might be passively using a social media sphere, and s/he may finally find him/herself with a changed mood depending on the nature of the content faced. Then, this good or bad mood is easily shared with others in a flash through the social media. Finally, as Alahmar (2016) described, social media exposes people especially the young generation to new exciting activities and events that may attract them and keep them engaged in different media contexts for hours just passing their time. It usually leads to reduced productivity, reduced academic achievement, and addiction to constant media use (Alahmar, 2016).
+`,
+`You found a hidden cookie! <br><img src="https://dictionary.cambridge.org/vi/images/thumb/biscui_noun_002_03480.jpg" alt="CookieImageExpectedHere">`
+];
+var titles = [
+    "Main",
+    "Report",
+    "Cookie!"
+]
+var navbtn = [
+    "Home",
+    "More",
+    "Cookie"
+]
