8890e6afc2b46be223b4b907516e676a16722952
nghuyen dinh quoc uyy <90595569+tonyuyndqOM@users.noreply.github.com>
2024-09-06 19:11:54 +0700

    COMMIT COMMIT COMMIT

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function openpage(n) {
     $("title").html(titles[n]);
 }
 $(() => {
+    $("#navbar").append(`<a class="iconcontent content" id="navicon"><div class="bimgic"></div></a>`);
+    $("#navicon").attr("href","javascript:openpage(0)");
     openpage(0);
     navbtn.forEach((v,i,a) => {
         $("#navbar").append(`<a class="content" id="nav${i}">${v}</a>`);
-        $(`#nav${i}`).attr("href",`javascript:openpage(${i})`);
+        $(`#nav${i}`).attr("href",`javascript:openpage(${i + 1})`);
     });
 });
